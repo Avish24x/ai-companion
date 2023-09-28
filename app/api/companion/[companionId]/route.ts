533fba0a252d793b6f5b94800fc874f675ef93a4
Milesh Taukoorah <mileshtaukoorah@gmail.com>
2023-09-28 14:45:29 +0400

    user can now edit delete companion added chat header

--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -1,24 +1,34 @@
 import prismadb from "@/lib/prismadb";
-import { currentUser } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
+
+// This function handles a PATCH request to update a companion's information.
 
 export async function PATCH(
   req: Request,
   { params }: { params: { companionId: string } }
 ) {
   try {
+    // Parse the request body to extract the updated companion data.
     const body = await req.json();
+
+    // Check if the user is authenticated and retrieve user information.
     const user = await currentUser();
+
+    // Extract relevant data fields from the request body.
     const { name, description, instructions, seed, src, categoryId } = body;
 
+    // Check if the 'companionId' parameter is provided in the request.
     if (!params.companionId) {
       return new NextResponse("Companion ID is required", { status: 400 });
     }
 
+    // Check if the user is authenticated and has required user data.
     if (!user || !user.id || !user.firstName) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
+    // Check if essential data fields are provided in the request.
     if (
       !src ||
       !name ||
@@ -27,11 +37,12 @@ export async function PATCH(
       !categoryId ||
       !seed
     ) {
-      return new NextResponse("missing required fields", { status: 400 });
+      return new NextResponse("Missing required fields", { status: 400 });
     }
 
-    // TODO: check for subcription
+    // TODO: Implement subscription check (Not implemented yet).
 
+    // Update the companion's information in the database using Prisma.
     const companion = await prismadb.companion.update({
       where: {
         id: params.companionId,
@@ -47,9 +58,36 @@ export async function PATCH(
         seed,
       },
     });
+
+    // Return the updated companion data in the response.
     return NextResponse.json(companion);
   } catch (error) {
+    // Handle any unexpected errors by logging them and returning an internal error response.
     console.log("[COMPANION_PATCH]", error);
-    return new NextResponse("internal error", { status: 500 });
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { companionId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const companion = await prismadb.companion.delete({
+      where: {
+        userId,
+        id: params.companionId,
+      },
+    });
+
+    return NextResponse.json(companion);
+  } catch (error) {
+    console.log("[COMPANION_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
 }

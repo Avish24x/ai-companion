cecd8e04bf4d363528ba61cc2a917298732584c1
Milesh Taukoorah <mileshtaukoorah@gmail.com>
2023-09-29 13:58:42 +0400

    fixed sidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,14 +4,14 @@ import { SheetContent, Sheet, SheetTrigger } from "@/components/ui/sheet";
 
 import { Sidebar } from "./sidebar";
 
-export const MobileSidebar = () => {
+export const MobileSidebar = ({ isPro }: { isPro: boolean }) => {
   return (
     <Sheet>
       <SheetTrigger>
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <Sidebar />
+        <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
   );
